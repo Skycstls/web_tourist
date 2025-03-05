765f4a7908b083e89fd435243ccccd5024c53f11
datadiego <juandiegomariscal@gmail.com>
2025-03-05 16:19:10 +0100

    update

--- scan.sh
+++ scan.sh
@@ -1,8 +1,13 @@
 rango=$1
-echo $rango
+rm -rf screenshots
+mkdir screenshots
 echo "Escaneando IPs"
-sudo masscan -p80,3000,8000,8080 $1 2>/dev/null | awk '{print $6 ":" $4}' | sed 's/\/tcp//' > /tmp/ips_raw.txt
+sudo masscan -p80 $1 2>/dev/null | awk '{print $6 ":" $4}' | sed 's/\/tcp//' > /tmp/ips_raw.txt
 
 echo "Creando screenshots"
-gowitness scan file -f /tmp/ips_raw.txt --screenshot-path ./screenshots
+#gowitness scan file -f /tmp/ips_raw.txt --screenshot-path ./screenshots
+gowitness scan file -f /tmp/ips_raw.txt --screenshot-path ./screenshots --status-codes 200,301,302
 
+echo "Check"
+echo "Numero de ips: $(cat /tmp/ips_raw.txt | wc -l)"
+echo "Numero de screenshots: $(ls screenshots | wc -l)"
